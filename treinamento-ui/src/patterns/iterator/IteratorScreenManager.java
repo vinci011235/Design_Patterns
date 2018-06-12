ab510f84d9061c4c3b725c5f71e39ace5603c471
vinci <elvis_soares@live.com>
2018-06-12 08:32:49 -0300

    Padrão Iterator criado

---
+++ treinamento-ui/src/patterns/iterator/IteratorScreenManager.java
@@ -0,0 +1,7 @@
+package patterns.iterator;
+
+public interface IteratorScreenManager {
+	//Interface Iterator para classe ScreenManager pacote ui.gui.jsx.utils
+	public Object next();
+	public boolean hasNext();
+}
